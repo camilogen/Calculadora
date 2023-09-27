7f1d62c6ccd24c506bb4047abb58f98515f16e48
camilogen <cam189gen189@gmail.com>
2023-09-26 21:08:58 -0300

    calculator estructure

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import '../hojas-de-estilo/Boton.css';
 
 function Boton(props) {
+
+  const esOperador = valor => {
+    return isNaN(valor) && (valor != '.') && (valor != '=');
+  };
+
   return (
-    <div>
+    <div 
+      className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`}>
       {props.children}
     </div>
   );
